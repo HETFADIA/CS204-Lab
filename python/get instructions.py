a9599c41941babcfedf3131653771770e8810537
Het <62541263+HETFADIA@users.noreply.github.com>
2021-03-21 13:31:15 +0530

    Update get instructions.py

--- python/get instructions.py
+++ python/get instructions.py
@@ -438,14 +438,115 @@ def bits_of_int_number(number):
     print("imm for S and B type :",bin_number[:7]+bin_number[20:25],twos_complement(bin_number[:7]+bin_number[20:25]))
     arr=[bin_number[:7],bin_number[7:12],bin_number[12:17],bin_number[17:20],bin_number[20:25],bin_number[25:]]
     print(arr)
+from riscv_assembler.convert import AssemblyConverter
+import pandas as pd
+
+
+def decode(code):
+    """
+
+    :param code:32 bit machine code in hex(without 0x32)
+    :return: dictionary of all data
+    """
+
+    fields = {}
+    df = pd.read_csv('instructions.csv')
+    #print(df)
+
+    code=hex(code)
+    machine_code = '0' * 32 + bin(int(code, 16))[2:]
+    machine_code = machine_code[-32:]
+    opcode = '0b'+machine_code[-7:]
+    df = df[df.opcode==opcode]
+    #print(df)
+    if len(df['id'])==0:
+        return 0
+    else:
+        format = list(df['format'])[0]
+
+        if format=='R':
+            funct3 = '0b'+machine_code[-15:-12]
+            funct7 = '0b'+machine_code[:7]
+            df = df[(df.funct3==funct3) & (df.funct7==funct7)]
+            #print(df)
+            if len(df['id']) == 0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['funct7']=list(df['funct7'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rs2']=machine_code[-25:-20]
+                fields['rd']=machine_code[-12:-7]
+
+
+        elif format=='I':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rd']=machine_code[-12:-7]
+                fields['immediate']=machine_code[0:12]
+
+
+        elif format=='S':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rs2']=machine_code[-25:-20]
+                fields['immediate']=machine_code[0:7]+machine_code[-12:-7]
+
+
+        elif format=='SB':
+            funct3 = '0b'+machine_code[-15:-12]
+            df = df[df.funct3==funct3]
+            if len(df['id'])==0:
+                return 0
+            else:
+                fields['neumonic']=list(df['neumonic'])[0]
+                fields['opcode']=list(df['opcode'])[0]
+                fields['funct3']=list(df['funct3'])[0]
+                fields['rs1']=machine_code[-20:-15]
+                fields['rs2']=machine_code[-25:-20]
+                fields['immediate']=machine_code[0]+machine_code[-8]+machine_code[1:7]+machine_code[-12:-8]      #0 not added in the end
+
+
+        elif format=='U':
+            fields['neumonic'] = list(df['neumonic'])[0]
+            fields['opcode']=opcode
+            fields['immediate']=machine_code[0:20]
+            fields['rd']=machine_code[-12:-7]
+
+
+        elif format=='UJ':
+            fields['neumonic'] = list(df['neumonic'])[0]
+            fields['opcode']=opcode
+            fields['immediate']=machine_code[0]+machine_code[-20:-12]+machine_code[-21]+machine_code[1:11]  #not shifted 12 bits see later
+            fields['rd']=machine_code[-12:-7]
+        return fields
+
+
 
 number=0x40A85793
 print(bits_of_int_number(number))
 while 1:
+    print("#########################################################")
     a=execute()
     print(a.checkFormat())
     print(a.decode())
-    
+    # print(decode(number))
     number=input()
     number="".join(i for i in number if (i!=" " and i!="\t"))
     if len(number)==32:
